e52d740dbe3b3813736342196d40f41246c91c3d
Garrett Mathers <garrett.mathers@gmail.com>
2024-02-15 22:32:15 -0600

    Fixed clock

--- A01/KoltenPulliamSerial.c
+++ A01/KoltenPulliamSerial.c
@@ -5,23 +5,26 @@
 
 int main() {
     unsigned long long sum = 0;
-
-    // Initialize the array
     int nums[N];
     for (int i = 0; i < N; i++) {
         nums[i] = i + 1;
     }
 
-    // Finding the sum of the addition of all values in the array
-    clock_t start = clock();
+    // For measuring wall clock time with high resolution
+    struct timespec start, end;
+
+    // Start timer
+    clock_gettime(CLOCK_REALTIME, &start);
+
     for (int i = 0; i < N; i++) {
         sum += nums[i];
     }
-    clock_t end = clock();
 
-    // Calculate time spent summing values
-    double time_spent_s = (double)(end - start) / CLOCKS_PER_SEC;
-    double time_spent_ms = time_spent_s * 1000;
+    // Stop timer
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    // Calculate time spent summing values. Convert to milliseconds.
+    double time_spent_ms = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec - start.tv_nsec) / 1000000.0;
 
     printf("The sum is: %lld\n", sum);
     printf("Time taken (milliseconds): %.2f\n", time_spent_ms);
